97aae69c3942dbf1e2e9b3e05934dfd018ac767d
leslieyi <leslieyi411@gmail.com>
2021-07-26 22:03:45 -0400

    Done? but still lost

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,25 +3,62 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+// function myFilter(myArray, myFilterCallback) {
+//   const newArray = [];
+//   for (const item of myArray) {
+//     if (myFilterCallback(item)) {
+//       newArray.push(item);
+//     }
+//   }
+//   return newArray;
+// }
+
+// function myMap(myArray, myMapCallback) {
+//   const newArray = [];
+//   for (const item of myArray) {
+//     const newItem = myMapCallback(item)
+//     newArray.push(newItem)
+//   }
+//   return newArray;
+// }
+
+// myFilter(allData, (item) => item.includes("whatever"))
+// allData.filter((item) => item.includes("whatever"))
+
+
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  const [allData, setAllData] = useState(items);
+  function onItemFormSubmit(itemNew) {
+    setAllData([...allData, itemNew]);
+  }
 
-  const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = allData.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  const [searchChange, setSearchChange] = useState("");
+  const handleSearchChange = (e) => setSearchChange(e.target.value);
+
+  const searchItemsToDisplay = itemsToDisplay.filter((item) =>
+    item.name.toLowerCase().includes(searchChange.toLowerCase())
+  );
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+      />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchItemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
